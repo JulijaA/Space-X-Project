4bf0777f4b30eb45e14cd3dc0ddf949e1f0cc205
Julija <julija.atanasovska13@gmail.com>
2021-11-28 09:32:41 +0100

    added the compiled build

---
+++ build/precache-manifest.b6a0daa8abd726d052d86a4a52a62330.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862a3341c3248770aae82ee19bf406c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b7f5921717ae6eafb4",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "2d09edd5888f7870c6a8",
+    "url": "/static/css/main.ce43e1e4.chunk.css"
+  },
+  {
+    "revision": "85b7f5921717ae6eafb4",
+    "url": "/static/js/2.e9e5208d.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e9e5208d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d09edd5888f7870c6a8",
+    "url": "/static/js/main.4e9a21e1.chunk.js"
+  },
+  {
+    "revision": "61365730246d13f2c245",
+    "url": "/static/js/runtime-main.a6c04937.js"
+  }
+]);
